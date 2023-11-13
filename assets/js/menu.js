b6e56ff9674165c514b981507434e886fc625142
ngoc160304 <ngocdev160304@gmail.com>
2023-11-13 21:46:33 +0700

    cập nhật logo lên trình duyệt

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,13 @@
 const menuBars = document.querySelector(".inner-bars");
 const listMenuBars = document.querySelector('.inner-menu-bar');
-console.log(menuBars);
-console.log(listMenuBars);
 
 var onresize = function(e) {
     width = document.body.clientWidth;
+    height = document.body.clientHeight;
     if(width >= 978) {
         listMenuBars.classList.remove("dp-block")
     }
- }
+}
 window.addEventListener("resize", onresize);
 menuBars.addEventListener("click", () => {
     listMenuBars.classList.toggle("dp-block")
